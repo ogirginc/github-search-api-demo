8b148a0bbbf557c2b8cfd066fa950ecead01c6e0
Oğulcan Girginç <ogulcan@girginc.com>
2024-03-16 11:12:59 +0300

    Configure vcr

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,10 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+require "vcr"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "test/vcr_cassettes"
+  config.hook_into :webmock
+end
